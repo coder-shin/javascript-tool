c68cbb532db9db8e874dc5bd9445d60d0ca6a1cf
newteak <invimacs@gmail.com>
2023-01-08 13:57:07 +0900

    Ref to snake case

--- string/decimal_seperators.js
+++ string/decimal_seperators.js
@@ -1,4 +1,4 @@
-const decimalSperators = (strs, ...args) => {
+const decimal_sperators = (strs, ...args) => {
   return args.reduce((a, c, i) => {
     return a + strs[i] + (c.toString()).replace(/\d{1,3}(?=(\d{3})+(?!\d))/g, '$&,')
   }, '') + strs[strs.length - 1]
@@ -7,6 +7,6 @@ const decimalSperators = (strs, ...args) => {
 const price = 300000000;
 const amount = 4;
 
-console.log(decimalSperators `Yesterday I bought ${amount} gucci bags.
+console.log(decimal_sperators `Yesterday I bought ${amount} gucci bags.
 It is a $${price} bag.
 I spend ${price * amount} dollars.`);
