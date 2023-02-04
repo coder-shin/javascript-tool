208f49c8411298099b67859d9f835114f6eb9c00
newteak <invimacs@gmail.com>
2023-02-04 11:58:07 +0900

    Ref add reverse test

--- src/string/reverse.js
+++ src/string/reverse.js
@@ -1,13 +1,5 @@
-console.log('reverse - best practice');
 const reverse = (s) =>  s.split('').reverse().join('');
 
-console.log(reverse('Hello, world!')); // !dlrow ,olleH
-console.log(reverse('안녕, 세계!')); // !계세 ,녕안
-console.log(reverse('おはよう、世界！')); // ！界世、うよはお
-console.log(reverse('')); // ''
-console.log();
-
-console.log('reverse_with_c_style');
 const reverse_with_c_style = (s) => {
   let revesed_s = '';
 
@@ -18,13 +10,6 @@ const reverse_with_c_style = (s) => {
   return revesed_s;
 }
 
-console.log(reverse_with_c_style('Hello, world!')); // !dlrow ,olleH
-console.log(reverse_with_c_style('안녕, 세계!')); // !계세 ,녕안
-console.log(reverse_with_c_style('おはよう、世界！')); // ！界世、うよはお
-console.log(reverse_with_c_style('')); // ''
-
-
-console.log('reverse_with_recursive');
 const reverse_with_recursive = (s) => {
   if (s === '') {
     return '';
@@ -33,7 +18,5 @@ const reverse_with_recursive = (s) => {
   }
 }
 
-console.log(reverse_with_recursive('Hello, world!')); // !dlrow ,olleH
-console.log(reverse_with_recursive('안녕, 세계!')); // !계세 ,녕안
-console.log(reverse_with_recursive('おはよう、世界！')); // ！界世、うよはお
-console.log(reverse_with_recursive('')); // ''
+export default { reverse, reverse_with_c_style, reverse_with_recursive };
+
